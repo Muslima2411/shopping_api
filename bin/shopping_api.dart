cf1589dab86f40cb0f87e41b545f59adedf20b65
muslima <muslima241105@gmail.com>
2023-11-27 10:36:23 +0500

    first commit

---
+++ bin/shopping_api.dart
@@ -0,0 +1,5 @@
+import 'package:shopping_api/shopping_api.dart' as shopping_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shopping_api.calculate()}!');
+}
---
+++ test/shopping_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:shopping_api/shopping_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
